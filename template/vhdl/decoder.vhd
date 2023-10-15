9bf8b40ae466b7d1177c6df096fcdc8f6aeb73b6
apana03 <andrei.pana@epfl.ch>
2023-10-15 20:37:58 +0300

    modified the Decoder to add a new cs_buttons output that is activated when we access the Buttons module (addresses 0x2030 to 0x2034)

--- template/vhdl/decoder.vhd
+++ template/vhdl/decoder.vhd
@@ -6,7 +6,8 @@ entity decoder is
         address : in  std_logic_vector(15 downto 0);
         cs_LEDS : out std_logic;
         cs_RAM  : out std_logic;
-        cs_ROM  : out std_logic
+        cs_ROM  : out std_logic;
+        cs_buttons : out std_logic
     );
 end decoder;
 
@@ -14,5 +15,6 @@ architecture synth of decoder is
 begin
     cs_ROM <= '1' when (unsigned(address) >= to_unsigned(0, 16)) and (unsigned(address) <= to_unsigned(4092, 16)) else '0';
     cs_RAM <= '1' when (unsigned(address) >= to_unsigned(4096, 16)) and (unsigned(address) <= to_unsigned(8188, 16)) else '0';
-    cs_LEDS <= '1' when (unsigned(address) >= to_unsigned(8192, 16)) and (unsigned(address) <= to_unsigned(8204, 16)) else '0';    
+    cs_LEDS <= '1' when (unsigned(address) >= to_unsigned(8192, 16)) and (unsigned(address) <= to_unsigned(8204, 16)) else '0';
+    cs_buttons <= '1' when (unsigned(address) >= to_unsigned(8240, 16)) and (unsigned(address) <= to_unsigned(8244, 16)) else '0';    
 end synth;
