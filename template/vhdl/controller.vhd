7b6253e707241709a53ed86fa9dc6c5c89f5881f
apana03 <andrei.pana@epfl.ch>
2023-10-22 14:01:12 +0300

    finished controller

--- template/vhdl/controller.vhd
+++ template/vhdl/controller.vhd
@@ -37,7 +37,7 @@ entity controller is
 end controller;
 
 architecture synth of controller is
-    type state_type is (fetch1, fetch2, decode, r_op, store, break, load1, load2, i_op, branch, call, callr, jmp, jmpi, ui_op, ri_op);
+    type state_type is (fetch1, fetch2, decode, r_op, store, break, load1, load2, i_op, branch, call, callr, jmp, jmpi, i_op_imm , r_op_small_imm);
     signal s_op, s_opx : std_logic_vector(7 downto 0);
     constant  r_type : std_logic_vector(7 downto 0) := X"3A";
     signal state, next_state : state_type;
@@ -73,12 +73,18 @@ begin
                         next_state <= callr;
                     elsif (s_opx = X"0D") or (s_opx = X"05") then
                         next_state <= jmp;
+                    -- check if opx detects an R-type instruction with small immediate
+                    elsif (s_opx = X"12") or (s_opx = X"1A") or (s_opx = X"3A") or (s_opx = X"02") then
+                        next_state <= r_op_small_imm; 
                     else
                         next_state <= r_op;
                     end if;
-                -- check if op = 0x04
-                elsif s_op = X"04" then
+                -- check if op signal detects an I-type instruction
+                elsif (s_op = X"04") or (s_op = X"08") or (s_op = X"10") or (s_op = X"18") or (s_op = X"20") then
                     next_state <= i_op;
+                -- check if op signal detects an I-type instruction with unsigned immediate
+                elsif (s_op = X"0C") or (s_op = X"14") or (s_op = X"1C") or (s_op = X"28") or (s_op = X"30") then
+                    next_state <= i_op_imm;
                 -- check if op = 0x17
                 elsif op = X"17" then
                     next_state <= load1;
@@ -103,19 +109,17 @@ begin
         end case;
     end process;
 
-
+    -- set outputs according to state
     read <= '1' when (state = fetch1) or (state = load1) else '0';
     pc_en <= '1' when (state = fetch2) or (state = call) or (state = callr) or (state = jmp) or (state = jmpi) else '0';
     ir_en <= '1' when state = fetch2 else'0' ;
-    rf_wren <= '1' when (state = i_op) or (state = r_op) or (state = load2) or (state = call) or (state = callr) else '0';
+    rf_wren <= '1' when (state = i_op) or (state = r_op) or (state = load2) or (state = call) or (state = callr) or (state = i_op_imm) or (state = r_op_small_imm) else '0';
     imm_signed <= '1' when (state = i_op) or (state = load1) else '0';
-    sel_rC <= '1' when state = r_op else '0';
+    sel_rC <= '1' when (state = r_op) or (state = r_op_small_imm) else '0';
     sel_b <= '1' when (state = r_op) or (state = store) or (state = branch) else '0';
     sel_addr <= '1' when (state = load1) or (state = store) else '0';
     sel_mem <= '1' when state = load2 else '0';
     write <= '1' when state = store else '0';
-
-    --set unused to 0 for now
     branch_op <= '1' when state = branch else '0';
     pc_add_imm <= '1' when state = branch else '0';
     pc_sel_a <= '1' when (state = callr) or (state = jmp) else '0';
@@ -123,26 +127,31 @@ begin
     sel_pc <= '1' when (state = call) or (state = callr) else '0';
     sel_ra <= '1' when (state = call) or (state = callr) else '0';
     
-    process(s_op, s_opx , op, opx)
+
+    -- asynch process to set alu op according to instruction
+    process(s_op, s_opx )
     begin
-        -- check if R-Type  
+        -- check if R-Type then map 3 MSB from opx to 3 LSB of op_alu
         if s_op = r_type then
             op_alu(2 downto 0) <= s_opx(5 downto 3);
         -- check this later
         elsif s_op = X"06" then 
             op_alu(2 downto 0) <= "100";
-        -- if I-Type
+        -- if I-Type then map 3 MSB from op to 3 LSB of op_alu
         else 
             op_alu(2 downto 0) <= s_op(5 downto 3);
         end if;
-
-        if s_opx = X"0E" then
+        
+        -- check if logical operation is needed
+        if (s_opx = X"1E") or (s_opx = X"16") or (s_opx = X"16") or (s_opx = X"06") or (s_opx = X"0E") or (s_op = X"0C") or (s_op = X"14") or (s_op = X"1C") then
             op_alu(5 downto 3) <= "100";
-        elsif s_opx = X"1B" then
+        elsif   (s_opx = X"02") or (s_opx = X"3A") or (s_opx = X"1A") or (s_opx = X"12") or (s_opx = X"0B") or (s_opx = X"03") or (s_opx = X"3B") or (s_opx = X"1B") or (s_opx = X"13") or (s_opx = X"1B") then
             op_alu(5 downto 3) <= "110";
-        elsif (s_op = X"04") or (s_op = X"17") or (s_op = X"15") then
+        elsif  (s_opx = X"31") or (s_op = X"04") or (s_op = X"17") or (s_op = X"15") or (s_op = X"04") then
             op_alu(5 downto 3) <= "000";
-        elsif (s_op = X"0E") or (s_op = X"16") or (s_op = X"1E") or (s_op = X"26") or (s_op = X"2E") or (s_op = X"36") then
+        elsif (s_opx = X"39") then
+            op_alu(5 downto 3) <= "001";
+        elsif (s_opx = X"30") or (s_opx = X"28") or (s_opx = X"20") or (s_opx = X"18") or (s_opx = X"10") or (s_opx = X"08") or (s_op = X"0E") or (s_op = X"16") or (s_op = X"1E") or (s_op = X"26") or (s_op = X"2E") or (s_op = X"36") or (s_op = X"08") or (s_op = X"10") or (s_op = X"18") or (s_op = X"20") or (s_op = X"28") or (s_op = X"30") then
             op_alu(5 downto 3) <= "011";
         end if;
     end process;
